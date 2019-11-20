53cc5e830c608b7dc099e60e20efa0055dd73c2c
Miss-Cheese <nat.galligan@gmail.com>
2019-11-19 21:30:55 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    # if self.end_with?(".")
+    #   return true
+    # else 
+    #   return false
+    # end
   end
 
   def question?
-
+    self.end_with?("?")
+  #     true 
+  #   else
+  #     false
+  #   end
   end
 
   def exclamation?
-
+    self.end_with?("!")
+    #   true
+    # else
+    #   false
+    # end
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject{|str|
+    str.empty?
+    }.count
+  # binding.pry
   end
 end
